c21583acaf20c68733e95c14825b7af3bec8ac39
MidhaTahir <midhatahirkhan2011@gmail.com>
2022-07-14 02:48:35 +0500

    fix: common js thing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db.js");
+const connectDB = require("./config/db");
 const reviewRoute = require("./routes/reviewRoute");
 
 dotenv.config();
